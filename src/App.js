ed775b09abf7ab8be9984f3bafc2eaa3e7ebcbcd
Timur Bykov <bikov.timur.013@gmail.com>
2022-02-07 12:57:16 +0300

    ADD:home-leftSide

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
+import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={<Login/>}/>
+          <Route path="/home" element={<><Header/><Home/></>}/>
         </Routes>
       </Router>
     </div>
